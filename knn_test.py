d1d38524d8f6559dc6359987b7ee3229cf760a92
Mikeias <mikeias66@gmail.com>
2017-10-16 04:38:03 -0300

    preenchimento dos compassos

--- knn_test.py
+++ knn_test.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
+import copy
 
-# 0: semicolcheia, 1: tercina, 2: colcheia, 3: seminima, 4: minima, 5: semibreve
-actual_durations = [0.25, 0.33, 0.5, 1, 2, 4]
+# 0: semicolcheia, 1: colcheia, 2: seminima, 3: minima, 4: semibreve
+actual_durations = [0.25, 0.5, 1, 2, 4]
+beats_by_compass = 4
+total_compasses = 16
 
 # Importing the dataset
 dataset = pd.read_csv('musics_mock.csv')
@@ -25,12 +28,27 @@ from sklearn.neighbors import KNeighborsClassifier
 classifier = KNeighborsClassifier()
 classifier.fit(X, y)
 
-X_test = pd.read_csv('x_test.csv', header=None).iloc[:, :].values
+
+result = np.array([[]])
+test_seq = pd.read_csv('x_test.csv', header=None).iloc[:, :].values
+X_test = copy.deepcopy(test_seq)
 X_test[:, 0] = labelencoder_X.transform(X_test[:, 0])
 X_test = onehotencoder.transform(X_test).toarray()
-X_test = sc.fit_transform(X_test)
+sc.fit(X_test)
 
-y_pred = classifier.predict(X_test)
-
-result = pd.read_csv('x_test.csv', header=None).iloc[:, :].values
-np.concatenate((result, np.array([y_pred]).T), axis=1)
+i = 1
+while(i <= total_compasses): 
+    compass_result = test_seq[test_seq[:, 1] == i, :]
+    
+    X_test = copy.deepcopy(compass_result)
+    X_test[:, 0] = labelencoder_X.transform(X_test[:, 0])
+    X_test = onehotencoder.transform(X_test).toarray()
+    X_test = sc.transform(X_test)
+    
+    y_pred = classifier.predict(X_test)
+    
+    compass_result = np.concatenate((compass_result, np.array([y_pred]).T), axis=1)
+    result = np.append(result, (compass_result))
+    result = np.reshape(result, (int(len(result)/3), 3))
+    
+    if sum([actual_durations[x] for x in result[result[:, 1] == i, 2]]) == beats_by_compass: i += 1
